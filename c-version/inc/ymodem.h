6abbe7d903491632ce034ec9096cfc1bb25ef917
yango3 <o3.yang@hotmail.com>
2023-03-18 16:04:59 +0800

    add cmake file

---
+++ c-version/inc/ymodem.h
@@ -0,0 +1,6 @@
+#ifndef Y_MODEM_H
+#define Y_MODEM_H
+
+int sync_with_client(int fd_port, char *buffer, int timeout_times);
+
+#endif
---
+++ c-version/src/ymodem.c
@@ -0,0 +1,7 @@
+#include "ymodem.h"
+
+int sync_with_client(int fd_port, char *buffer, int timeout_times)
+{
+
+  return 0;
+}
