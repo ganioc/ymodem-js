6b7ee5306f99e7b42368a2ac58c8753b6416f478
yango3 <o3.yang@hotmail.com>
2023-03-23 14:04:26 +0800

    update dd bin

---
+++ bin/dd/merge.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "merge 2 bins"
+
+PROJSTR="lamp421"
+echo "${PROJ_STR}"
+
+FILESIZE=$(expr 64 \* 1024)
+DATESTR=$(date +%m%d-%H%M)
+
+#OUTPUT="${PROJSTR}-combo-${DATESTR}.bin"
+OUTPUT="lamp421-all-0323.bin"
+
+INFILE1="../lampbl421-0323-none.bin"
+INFILE2="../lamp421-0320.bin"
+
+INFILE1_SIZE=$(ls -l "${INFILE1}" | awk '{print $5}')
+INFILE2_SIZE=$(ls -l "${INFILE2}" | awk '{print $5}')
+
+OFFSET=$(expr 16 \* 1024)
+
+echo "Output file is:" "${OUTPUT}"
+echo "Input file 1 is:" "$INFILE1_SIZE"
+echo "Input file 2 is:" "$INFILE2_SIZE"
+echo "Offset is:" "${OFFSET}"
+
+echo "Create a ${FILESIZE} byte bin"
+</dev/zero head -c ${FILESIZE} | tr '\0' '\377' > "${OUTPUT}"
+
+#dd conv=notrunc if="${INFILE1}" of="${OUTPUT}" bs=1 count="${INFILE1_SIZE}" seek=0
+
+
+
+dd conv=notrunc if="${INFILE2}" of="${OUTPUT}" bs=1 count="${INFILE2_SIZE}" seek="${OFFSET}"
+
+sync
+
+dd conv=notrunc if="${INFILE1}" of="${OUTPUT}" bs=1 count="${INFILE1_SIZE}" seek=0
